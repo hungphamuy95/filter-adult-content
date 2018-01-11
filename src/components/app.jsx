d5d43415dc040cd29b74d4ad16bed7cd1e6b155f
hungphamuy95 <hungpham.huy95@gmail.com>
2018-01-11 16:47:21 +0700

    change content of app component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 export default class App extends React.Component{
     render(){
         return(
-            <div><h1>Hello World</h1></div>
+            <div><strong>Hello World</strong></div>
         )
     }
 }
